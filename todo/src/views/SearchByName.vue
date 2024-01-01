c638d83be85d32cf25fd170d7848ac6634136bce
Jahidul Islam <56561463+sammijahidul@users.noreply.github.com>
2024-01-02 01:31:46 +0600

    fatching data and viewed in a card template

--- todo/src/views/SearchByName.vue
+++ todo/src/views/SearchByName.vue
@@ -8,8 +8,24 @@
             @change="searchMeals"
         />
     </div>
-    <div>
-        <pre>{{ meals }}</pre>
+    <!--Crated a meal-item template-->
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+        <div v-for="meal in meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
+            <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl w-full h-48 object-cover">
+            <div class="p-3 ">
+                <h3 class="font-bold">{{ meal.strMeal }}</h3>
+                <p class="mb-4">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque nobis animi officia doloribus.
+                </p>
+                <div>
+                    <a :href="meal.strYoutube" target="_blank" class="px-3 py-2 rounded border-2 border-red-600 hover:bg-red-600 hover:text-white transition-colors">YouTube</a>
+                    <router-link to="/">
+                        View
+                    </router-link>
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
 
